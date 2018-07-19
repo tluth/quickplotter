eb4f88c89202eb22b209851dd2c2e6eca8a96852
tluth <35136057+tluth@users.noreply.github.com>
2018-07-19 22:14:32 +0200

    Add files via upload

---
+++ quickplotter.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import csv
+import matplotlib.pyplot as plt
+import pandas as pd
+import argparse
+
+# Parse command line arguments
+parser = argparse.ArgumentParser(
+    description='Train Mask R-CNN to detect balloons.')
+parser.add_argument('x',
+                    help="Header name for desired x variable")
+parser.add_argument('y',
+                    help="Header name for desired y variable")
+parser.add_argument("-f", "--filepath",
+                    action='append',
+                    help='<Required> Set flag',
+                    required=True)
+args = parser.parse_args()
+
+plt.figure(figsize = (20, 10))
+
+x = args.x
+y = args.y
+
+for i in range(len(args.filepath)):
+    filepath = args.filepath[i]
+    df = pd.read_csv(filepath, header = 0)
+    plt.plot(df[x], df[y])
+
+plt.savefig("./my_plot")
+plt.show()
